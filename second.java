f54e2c2c18789f46fbfccc422dfdaff744b56b88
Chhatra Neupane <156672137+chhatraraj@users.noreply.github.com>
2024-06-27 10:17:31 +0545

    Update second.java

--- second.java
+++ second.java
@@ -5,7 +5,7 @@ class second {
     public static void main ( String [] args) throws Exception {
         Socket s = new Socket("localhost",4000);
     Scanner scan = new Scanner(System.in);
-    System.out.println("Enter the numbrt");
+    System.out.println("Enter the number");
     String msg = scan.next();
     DataInputStream dis = new DataInputStream(s.getInputStream());
     DataOutputStream dos = new DataOutputStream(s.getOutputStream());
